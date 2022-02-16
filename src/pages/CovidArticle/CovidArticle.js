f8580d70aa40d724623efc7938dafa3f290f4025
dbwk18 <dbwk18@kaist.ac.kr>
2022-02-16 23:38:23 +0900

    build base article

---
+++ src/pages/CovidArticle/CovidArticle.js
@@ -0,0 +1,73 @@
+import * as d3 from 'd3';
+import React, {useState, useEffect, useRef} from 'react';
+import Papa from 'papaparse';
+
+import NYTHeader from '../../images/NYT/NYTHeader.png'
+import NYTTitle from '../../images/NYT/NYTTitle.png'
+import NYTBody1 from '../../images/NYT/NYTBody1.png'
+import NYTBody2 from '../../images/NYT/NYTBody2.png'
+import NYTBody3 from '../../images/NYT/NYTBody3.png'
+import NYTBody4 from '../../images/NYT/NYTBody4.png'
+import NYTBody5 from '../../images/NYT/NYTBody5.png'
+import NYTBody6 from '../../images/NYT/NYTBody6.png'
+import NYTBody7 from '../../images/NYT/NYTBody7.png'
+import NYTBody8 from '../../images/NYT/NYTBody8.png'
+import NYTFooter from '../../images/NYT/NYTFooter.png'
+
+function CovidGraph() {
+
+    const [parsedData, setParsedData] = useState([]);
+
+    const graphRef = useRef();
+
+    const parseFile =  (file) => {
+        Papa.parse('../../data/covid_confirmed_usafacts.csv', {
+            header: true,
+            complete: results => {
+                console.log(results)
+                setParsedData(results.data)
+            }
+        })
+    }
+
+    useEffect(()=> {
+        parseFile();
+    }, [])
+
+   
+    const draw = () => {
+
+    }
+
+
+    
+    return(
+        <React.Fragment>
+            <div ref={graphRef} className='GraphContainer' style={{backgroundColor: "aliceblue", width: "100%", height: "350px"}}/>
+        </React.Fragment>
+    )
+}
+
+function CovidArticle() {
+
+    return (
+        <div>
+            <img src={NYTHeader} width='100%' />
+            <img src={NYTTitle} width='100%' />
+
+            <CovidGraph />
+            <img src={NYTBody1} width='100%' />
+            <img src={NYTBody2} width='100%' />
+            <img src={NYTBody3} width='100%' />
+            <img src={NYTBody4} width='100%' />
+            <img src={NYTBody5} width='100%' />
+            <img src={NYTBody6} width='100%' />
+            <img src={NYTBody7} width='100%' />
+            <img src={NYTBody8} width='100%' />
+            <img src={NYTFooter} width='100%' />
+        </div>
+    )
+}
+
+export default CovidArticle;
+
