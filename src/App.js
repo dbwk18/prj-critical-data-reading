f554368bf320d8dd000a2db0f6c454445bba258d
Hyunwoo Kim <khw0726@kaist.ac.kr>
2023-03-08 22:23:51 +0900

    add carbon article

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import CovidArticle from './pages/CovidArticle';
 import UnemployArticle from './pages/UnemployArticle';
 import CPIArticle from './pages/CPIArticle';
 import CPIArticle2 from './pages/CPIArticle2'
+import CarbonArticle from './pages/CarbonArticle'
+
 import UserLogin from './pages/UserLogin/';
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
         {/* <Route path="/" element={<CPIArticle />} /> */}
         <Route path="/prj-critical-data-reading" element={<UserLogin />} />
         <Route path="/nyt-cpi-article" element={<CPIArticle2 />} />
+        <Route path="/nyt-carbon-article" element={<CarbonArticle />} />
       </Routes>
     </BrowserRouter>
   );
